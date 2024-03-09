db086d9dd342733b6dad0de2db3372730562ac01
santiagosayshey <chaucted@gmail.com>
2024-03-10 05:41:15 +1030

    Update helpers.h with comments to document new functions

--- helpers.h
+++ helpers.h
@@ -38,13 +38,14 @@ extern bool Parse_HTTP_Request(int, struct http_request *);
 extern bool Is_Valid_Resource(char *);
 
 /*----------------------------------------------------------
- * Function: void Send_Resource(int socket, char * URI) 
+ * Function: void Send_Resource(int socket, char *URI, const char *request_method)
  *
  * Purpose:  Sends the contents of the file referred to in URI on the socket
  *
- * Parameters:  socket  : the socket to send the content on
- *                URI   : the Universal Resource Locator, both absolute and 
- *                        relative URIs are accepted
+ * Parameters:  socket         : the socket to send the content on
+ *              URI            : the Universal Resource Locator, both absolute and
+ *                               relative URIs are accepted
+ *              request_method : the HTTP request method (e.g., "GET", "POST")
  *
  * Returns:  void - errors will cause exit with error printed to stderr
  *
@@ -53,7 +54,34 @@ extern bool Is_Valid_Resource(char *);
 
 extern void Send_Resource(int socket, char *URI, const char *request_method);
 
+/*----------------------------------------------------------
+ * Function: void Send_Error_Response(int socket, int status_code, const char *status_phrase)
+ *
+ * Purpose:  Sends an error response with the specified status code and phrase to the client
+ *
+ * Parameters:  socket        : the socket to send the response on
+ *              status_code   : the HTTP status code to send
+ *              status_phrase : the corresponding status phrase for the status code
+ *
+ * Returns:  void
+ *
+ *-----------------------------------------------------------
+ */
+
 extern void Send_Error_Response(int socket, int status_code, const char *status_phrase);
+
+/*----------------------------------------------------------
+ * Function: void Get_Content_Type(const char *resource, char *content_type)
+ *
+ * Purpose:  Determines the content type based on the file extension of the resource
+ *
+ * Parameters:  resource     : the path or URI of the resource
+ *              content_type : the buffer to store the determined content type
+ *
+ * Returns:  void
+ *
+ *-----------------------------------------------------------
+ */
 
 extern void Get_Content_Type(const char *resource, char *content_type);
 
